917e777bba1dc7d76cd330007d608734d9b4f119
Muhammad Ridwan <muh.ridwan.777@gmail.com>
2021-06-24 17:46:33 +0700

    update styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,11 @@ if(navClose) {
     navMenu.classList.remove('show-menu')
   })
 }
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
